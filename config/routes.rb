33f4909c89319ec07a8c415b80536e628ecb5edf
André <andre.szlima1@gmail.com>
2020-07-04 12:50:40 -0300

    CRUD concluído pra People Create Read Update Delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'people/update'
   get 'people/destroy'
   get 'home/index'
+
+  resources :people
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   root 'home#index'
